044b5a4a51c0e2629c695a0b46ca133b8edbab12
s-grundner <simon.grundner@gmail.com>
2025-10-02 01:49:53 +0200

    add midi submodule

--- src/synth.v
+++ src/synth.v
@@ -34,16 +34,16 @@ module synth (
         .midiData_o(midiByte)
     );
 
-    // midi #(
-    //     .MIDI_CHANNEL(0)
-    // ) midi_inst (
-    //     .clk_i(clk_i),
-    //     .nrst_i(nrst_i),
-    //     .midiByte_i(midiByte),
-    //     .midiByteValid_i(midiByteValid),
-    //     .note_o(note),
-    //     .noteOnStrb_o(noteOnStrb),
-    //     .noteOffStrb_o(noteOffStrb)
-    // );
+    midi #(
+        .MIDI_CHANNEL(0)
+    ) midi_inst (
+        .clk_i(clk_i),
+        .nrst_i(nrst_i),
+        .midiByte_i(midiByte),
+        .midiByteValid_i(midiByteValid),
+        .note_o(note),
+        .noteOnStrb_o(noteOnStrb),
+        .noteOffStrb_o(noteOffStrb)
+    );
 
 endmodule
